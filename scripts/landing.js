ed10ccf450e1b0fab4fda3b0b7d5690dececbb02
jbag123 <joshcone@googlemail.com>
2017-06-01 20:19:53 +0100

    redo of assignment 8

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,41 @@
 var pointsArray = document.getElementsByClassName('point');
 
-function revealPoint(points) {
-        var pointsTitle = document.getElementsByClassName('point-title');
-        var pointsSymbols = document.querySelectorAll('.ion-music-note, .ion-radio-waves, .ion-iphone');
-        for (var i = 0; i < points.length; i++ ) {
-                points[i].style.opacity = 1;
-                points[i].style.background = "linear-gradient(rgb(58,23,63), rgba(0, 255, 255, 0.5 95%)";
-                points[i].style.color = "rgb(58,23,63)";
-                points[i].style.transform = "scaleX(1) translateY(0)";
-                points[i].style.msTransform = "scaleX(1) translateY(0)";
-                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function(points) {
+        function revealPoint(index) {
+                points[index].style.opacity = 1;
+                points[index].style.background = "linear-gradient(rgb(58,23,63), rgba(0, 255, 255, 0.5 95%)";
+                points[index].style.color = "rgb(58,23,63)";
+                points[index].style.transform = "scaleX(1) translateY(0)";
+                points[index].style.msTransform = "scaleX(1) translateY(0)";
+                points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
         }
-        for (var i = 0; i < pointsTitle.length; i++ ) {
-                pointsTitle[i].style.borderBottom = "1px solid rgb(58,23,63)";
-        }
-        for (var i = 0; i < pointsSymbols.length; i++ ) {
-                pointsSymbols[i].style.color = "rgb(233,50,117)";
+        for (var i = 0; i < points.length; i++) {
+                revealPoint(i);
         }
 };
 
+        // var pointsTitle = document.getElementsByClassName('point-title');
+        // var pointsSymbols = document.querySelectorAll('.ion-music-note, .ion-radio-waves, .ion-iphone');
+        // for (var i = 0; i < points.length; i++ ) {
+        //
+        // }
+        // for (var i = 0; i < pointsTitle.length; i++ ) {
+        //         pointsTitle[i].style.borderBottom = "1px solid rgb(58,23,63)";
+        // }
+        // for (var i = 0; i < pointsSymbols.length; i++ ) {
+        //         pointsSymbols[i].style.color = "rgb(233,50,117)";
+        // }
+// };
+
 window.onload = function () {
         if (window.innerHeight > 950) {
-                revealPoint(pointsArray);
+                animatePoints(pointsArray);
         }
         var sellingPoints = document.getElementsByClassName('selling-points')[0];
         var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
         window.addEventListener('scroll', function(event) {
                 if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-                        revealPoint(pointsArray);
+                        animatePoints(pointsArray);
                 }
         });
 }
