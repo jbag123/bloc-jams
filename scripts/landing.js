b7a6d8dc36fdf0f0baacded8a0513fa13542b574
jbag123 <joshcone@googlemail.com>
2017-06-28 22:15:06 +0100

    commiting checkpoint-16-jquery-landing-page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,34 +1,28 @@
-var animatePoints = function(points,pointsTitle,pointsSymbols) {
-        function revealPoint(index) {
-                points[index].style.opacity = 1;
-                points[index].style.background = "linear-gradient(rgb(58,23,63), rgba(0, 255, 255, 0.5 95%)";
-                points[index].style.color = "rgb(58,23,63)";
-                points[index].style.transform = "scaleX(1) translateY(0)";
-                points[index].style.msTransform = "scaleX(1) translateY(0)";
-                points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-                pointsTitle[index].style.borderBottom = "1px solid rgb(58,23,63)";
-                pointsSymbols[index].style.color = "rgb(233,50,117)";
-        }
-        
-        forEach(points, revealPoint);
-        forEach(pointsTitle, revealPoint);
-        forEach(pointsSymbols, revealPoint);
-};
+var animatePoints = function() {
+        var revealPoint =function() {
+                $(this),css( {
+                        opacity: 1,
+                        transform: 'scaleX(1) translateY(0)'
+                });
+        };
 
-window.onload = function () {
+        $.each($('.point'), revealPoint);
+};
+$(window).load(function() {
 
         var pointsArray = document.getElementsByClassName('point');
         var pointsTitleArray = document.getElementsByClassName('point-title');
         var pointsSymbolsArray = document.querySelectorAll('.ion-music-note, .ion-radio-waves, .ion-iphone');
 
-        if (window.innerHeight > 950) {
-                animatePoints(pointsArray,pointsTitleArray,pointsSymbolsArray);
+        if ($(window).height() > 950) {
+                animatePoints();
         }
-        var sellingPoints = document.getElementsByClassName('selling-points')[0];
-        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-        window.addEventListener('scroll', function(event) {
-                if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-                        animatePoints(pointsArray,pointsTitleArray,pointsSymbolsArray);
+
+        var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+        $(window).scroll(function(event)) {
+                if ($(window).scrollTop() >= scrollDistance) {
+                        animatePoints();
                 }
         });
-}
+});
